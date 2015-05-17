502f6285dac3cc88b77fe63015180df8ad09e1d2
Totty <webdesignporto@gmail.com>
2015-05-17 23:33:43 +0100

    Adds more edge cases tests.

--- test/TextAnalyzer.js
+++ test/TextAnalyzer.js
@@ -9,6 +9,10 @@ describe('TextAnalyzer', function(){
     textAnalyzer = new TextAnalyzer();
   })
 
+  it('should return 0 vowels by default', function(){
+    expect(textAnalyzer.getVowels()).to.be.equal(0);
+  })
+
   it('should return the number of consonants that appear in the text', function(){
     textAnalyzer.setText(text);
     expect(textAnalyzer.getConsonants()).to.be.equal(21);
@@ -30,7 +34,7 @@ describe('TextAnalyzer', function(){
   })
 
   it('should return the top 3 consonants and how many times they appear in the text.', function(){
-    var text2 = '5 * aAaaa, 3 * aaA, eEe, ii, o, 5 * BBBbB, 2 * ccC, 2 * dd, 3 * BBb, f'
+    var text2 = '5 * aAaaa, 3 * aaA, eEe, ii, o, 5 * BBBbB, 2 * ccC, 2 * dd, 3 * BBb, f';
     textAnalyzer.setText(text2);
     var top3Consonants = textAnalyzer.getTopConsonants(3);
 
@@ -45,7 +49,7 @@ describe('TextAnalyzer', function(){
   })
 
   it('should return the top 3 vowels used and how many times they appear in the text.', function(){
-    var text2 = '5 * aAaaa, 3 * aaA, eEe, ii, o, 5 * BBBbbB, 2 * ccC, 2 * dd, 3 * BBb, f'
+    var text2 = '5 * aAaaa, 3 * aaA, eEe, ii, o, 5 * BBBbbB, 2 * ccC, 2 * dd, 3 * BBb, f';
     textAnalyzer.setText(text2);
     var top3Vowels = textAnalyzer.getTopVowels(3);
 
@@ -56,6 +60,14 @@ describe('TextAnalyzer', function(){
     expect(top3Vowels[1].times).to.be.equal(3);
     expect(top3Vowels[2].letter).to.be.equal('i');
     expect(top3Vowels[2].times).to.be.equal(2);
+  })
+
+  it('should return stats about the last text set', function(){
+    textAnalyzer.setText('a');
+    expect(textAnalyzer.getVowels()).to.be.equal(1);
+    textAnalyzer.setText('b');
+    expect(textAnalyzer.getVowels()).to.be.equal(0);
+    expect(textAnalyzer.getConsonants()).to.be.equal(1);
   })
 
 })
