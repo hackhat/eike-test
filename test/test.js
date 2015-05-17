3d5909810c0ef5f2456f6f9dd00d65e962c0b333
Totty <webdesignporto@gmail.com>
2015-05-17 22:13:13 +0100

    Adds more tests.

--- test/test.js
+++ test/test.js
@@ -1,21 +1,44 @@
 var expect = require('chai').expect;
 describe('text analyzer', function(){
   var TextAnalyzer = require('../src/TextAnalyzer');
-  // A text file would be better but this works
-  // Consonants: 153;
-  // Vowels: 100;
-  var text = 'I am a software engineer with broad experience in different roles of the full software development cycle. I work on requirements, functional design, technical design (architecture) and programming of applications and systems. I have experience as a Scrum Master or team lead and enjoy coaching colleagues.'
+  var text = 'I am a software engineer with broad experience...'
+  var textAnalyzer;
+
+  beforeEach(function(done){
+    textAnalyzer = new TextAnalyzer();
+  })
 
   it('should return the number of consonants that appear in the text', function(){
-    var textAnalyzer = new TextAnalyzer();
     textAnalyzer.setText(text);
-    expect(textAnalyzer.getConsonants()).to.be(153);
+    expect(textAnalyzer.getConsonants()).to.be(21);
   })
 
   it('should return the number of vowels that appear in the text', function(){
+    textAnalyzer.setText(text);
+    expect(textAnalyzer.getVowels()).to.be(21);
   })
 
   it('should return the top 3 consonants and top 3 vowels used and how many times they appear in the text.', function(){
+    var text2 = '5 * aAaaa, 3 * aaA, eEe, ii, o, 5 * BBBbbB, 2 * ccC, 2 * dd, 3 * BBb, f'
+    textAnalyzer.setText(text2);
+    var top3Consonants = textAnalyzer.getTopConsonants(3);
+    var top3Vowels     = textAnalyzer.getTopVowels(3);
+
+    expect(top3Consonants.length).to.be(3);
+    expect(top3Consonants[0].letter).to.be('b');
+    expect(top3Consonants[0].times).to.be(8);
+    expect(top3Consonants[1].letter).to.be('c');
+    expect(top3Consonants[1].times).to.be(3);
+    expect(top3Consonants[2].letter).to.be('d');
+    expect(top3Consonants[2].times).to.be(2);
+
+    expect(top3Vowels.length).to.be(3);
+    expect(top3Vowels[0].letter).to.be('a');
+    expect(top3Vowels[0].times).to.be(8);
+    expect(top3Vowels[1].letter).to.be('e');
+    expect(top3Vowels[1].times).to.be(3);
+    expect(top3Vowels[2].letter).to.be('i');
+    expect(top3Vowels[2].times).to.be(2);
   })
 
 })
