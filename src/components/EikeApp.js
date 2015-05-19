905ec6db874f0466f7df4a823ea69f4f88f00272
Totty <webdesignporto@gmail.com>
2015-05-19 11:09:50 +0100

    Finishes UI.

--- src/components/EikeApp.js
+++ src/components/EikeApp.js
@@ -1,20 +1,37 @@
 'use strict';
-var TextAnalyzer = require('../TextAnalyzer');
+require('styles/main.css');
+var TextAnalyzer = require('./TextAnalyzer');
 var textAnalyzer = new TextAnalyzer();
 
-var inputEl          = document.getElementById('input');
-var consonantsEl     = document.getElementById('consonants');
-var vowelsEl         = document.getElementById('vowels');
-var top3consonantsEl = document.getElementById('top-3-consonants');
-var top3vowelsEl     = document.getElementById('top-3-vowels');
+var inputEl         = document.getElementById('input');
+var consonantsEl    = document.getElementById('consonants-output');
+var vowelsEl        = document.getElementById('vowels-output');
+var topConsonantsEl = document.getElementById('top-consonants');
+var topVowelsEl     = document.getElementById('top-vowels');
+
+var removeChildren = function(el){
+	while(el.firstChild){
+	    el.removeChild(el.firstChild);
+	}
+}
+
+var createLetterChildren = function(parentEl, lettersStats){
+	removeChildren(parentEl);
+	textAnalyzer.getTopConsonants(3).forEach(function(letterStats){
+		var el = document.createElement("span");
+		el.innerHTML = letterStats.letter + ': ' + letterStats.times + ' times' ;
+		parentEl.appendChild(el);
+	})
+}
 
 var update = function(){
 	textAnalyzer.setText(inputEl.value);
-	consonantsEl.innerHTML     = textAnalyzer.getConsonants();
-	vowelsEl.innerHTML         = textAnalyzer.getVowels();
-	top3consonantsEl.innerHTML = JSON.stringify(textAnalyzer.getTopConsonants(3));
-	top3vowelsEl.innerHTML     = JSON.stringify(textAnalyzer.getTopVowels(3));
+	consonantsEl.innerHTML    = textAnalyzer.getConsonants();
+	vowelsEl.innerHTML        = textAnalyzer.getVowels();
+	createLetterChildren(topConsonantsEl, textAnalyzer.getTopConsonants(3))
+	createLetterChildren(topVowelsEl, textAnalyzer.getTopVowels(3))
 }
+update();
 
 inputEl.onkeyup = function(e){
 	update();
